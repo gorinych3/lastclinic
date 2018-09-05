367fd5e1c087fa0534c60fed9bafcc44f634d309
gorinych3 <gorinych33@yandex.ru>
2018-09-05 19:28:07 +0500

    BD

--- src/main/java/ru/egor/webClinic/servlets/CreateUserJsonServlet.java
+++ src/main/java/ru/egor/webClinic/servlets/CreateUserJsonServlet.java
@@ -1,7 +1,8 @@
 package ru.egor.webClinic.servlets;
 
 import org.codehaus.jackson.map.ObjectMapper;
-import ru.egor.clinic.se.User;
+import ru.egor.connection.UserDaoImpl;
+import ru.egor.pojo.User;
 import ru.egor.webClinic.servlets.form.UserForm;
 import ru.egor.webClinic.servlets.store.UserCache;
 
@@ -11,10 +12,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class CreateUserJsonServlet extends HttpServlet {
 
     private final UserCache USER_CACHE = UserCache.getINSTANCE();
+    private UserDaoImpl userDao = new UserDaoImpl();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -22,13 +25,18 @@ public class CreateUserJsonServlet extends HttpServlet {
         final ServletOutputStream out = resp.getOutputStream();
         out.print(new ObjectMapper().writeValueAsString(USER_CACHE.values()));
         out.flush();
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.addHeader("Content-Type", "application/json; charset=utf-8");
         final UserForm form = new ObjectMapper().readValue(req.getInputStream(), UserForm.class);
-        USER_CACHE.add(new User(USER_CACHE.generateId(), form.getLogin(), null));
+        try {
+            userDao.addUser(new User(form.getLogin(), "0000"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         resp.getOutputStream().write("{'result' : 'true'}".getBytes());
     }
 }
